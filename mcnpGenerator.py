f631e39d624d29dfcd4c444ded54e0726b33c6f2
Dustin Woolsey <dwoolsey@ksu.edu>
2017-03-29 16:31:36 -0500

    Read assembly rod positions now

--- mcnpGenerator.py
+++ mcnpGenerator.py
@@ -1,10 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import sin, cos, linspace, array, pi
+import csv
+
+
 
 
 letter = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7}
 
+
+def read_rod_pos():
+    x = []
+    y = []
+
+
+
+    with open('hex_from_sat_4.txt', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ')
+        for row in spamreader:
+            if ((int(row[2]) - 1) % 3) == 0:
+                x_p = float(row[10])
+                y_p = float(row[11])
+
+                if abs(x_p) <= 1E-10:
+                    x_p = 0
+
+                if abs(y_p) <= 1E-10:
+                    y_p = 0
+
+
+
+                x.append(x_p)
+                y.append(y_p)
+
+
+
+
+    plt.plot(x, y, 'bo')
+    plt.show()
+
+    return x, y
+
+
+def get_all_rod_pos():
+    x_1, y_1 = read_rod_pos()
+
+    x_2 = x_1
+    y_2 = [item - 14.7 for item in y_1]
+
+    x_3 = x_1
+    y_3 = [item + 14.7 for item in y_1]
+
+    plt.plot(x_1, y_1, 'bo', x_2, y_2, 'bo' , x_3, y_3, 'bo')
+    plt.show()
 
 def get_assembly_rod_pos(n_rings, fuel_pitch):
     '''
@@ -17,9 +65,10 @@ def get_assembly_rod_pos(n_rings, fuel_pitch):
     x = [0]
     y = [0]
 
+    vert_dist = sin(60) * fuel_pitch
+    hor_dist = cos(60) * fuel_pitch
 
-
-    for ring in range(n_rings):
+    for ring in range(n_rings):    # rings range from 0 to n_rings - 1
 
 
 
@@ -29,28 +78,83 @@ def get_assembly_rod_pos(n_rings, fuel_pitch):
         else:
 
             #Generate elements along symmetry lines
-            x.append(fuel_pitch * n_rings)
+            x.append(fuel_pitch * ring)
             y.append(0)
 
-            x.append( - fuel_pitch * n_rings)
+            x.append( - fuel_pitch * ring)
             y.append(0)
 
-            x.append(cos(60) * fuel_pitch * n_rings)
-            y.append(sin(60) * fuel_pitch * n_rings)
+            x.append(hor_dist * ring)
+            y.append(vert_dist * ring)
 
-            x.append(-cos(60) * fuel_pitch * n_rings)
-            y.append(sin(60) * fuel_pitch * n_rings)
+            x.append(-hor_dist * ring)
+            y.append(vert_dist * ring)
 
-            x.append(cos(60) * fuel_pitch * n_rings)
-            y.append(-sin(60) * fuel_pitch * n_rings)
+            x.append(hor_dist * ring)
+            y.append(-vert_dist * ring)
 
-            x.append(-cos(60) * fuel_pitch * n_rings)
-            y.append(-sin(60) * fuel_pitch * n_rings)
+            x.append(-hor_dist * ring)
+            y.append(-vert_dist * ring)
 
+
+            #generate elements on horizontal planes
+
+            n = None
+
+            if ring > 1:
+                n = ring - 1
+
+
+            if n and n % 2 == 0:    # IF EVEN
+
+                cyd = int(n / 2)
+
+                for num_d in range(cyd):
+                    num_d = (num_d * 2) +  1
+
+                    x.append(num_d * hor_dist)
+                    y.append(vert_dist * ring)
+
+                    x.append(-num_d * hor_dist)
+                    y.append(vert_dist * ring)
+
+                    x.append(num_d * hor_dist)
+                    y.append(-vert_dist * ring)
+
+                    x.append(-num_d * hor_dist)
+                    y.append(-vert_dist * ring)
+
+
+
+            elif n and n % 2 != 0:   # IF ODD
+                x.append(0)
+                y.append(vert_dist * ring)
+
+                x.append(0)
+                y.append(-vert_dist * ring)
+
+                cyc = int(n / 2)
+
+                for num_c in range(cyc):
+                    num_c += 1
+                    x.append(num_c * hor_dist * 2 )
+                    y.append(vert_dist * ring)
+
+                    x.append(-num_c * hor_dist * 2 )
+                    y.append(vert_dist * ring)
+
+                    x.append(num_c * hor_dist * 2 )
+                    y.append(-vert_dist * ring)
+
+                    x.append(-num_c * hor_dist * 2 )
+                    y.append(-vert_dist * ring)
+
+
+            else:
+                pass
             #generate intermediate elements
 
-            if ring - 1 != 0:
-                n = ring - 1
+
 
                 angle = 60 / n      # angle between rods on side of hexagon (Distance is pitch)
 
@@ -81,12 +185,4 @@ def write_file(outputName, s):
 
 
 if __name__ == '__main__':
-
-
-    s_test = 'Lorem ipsum dolor sit amet, eu vix tamquam definitiones, ad mel eligendi postulant.' \
-             ' \nVix ut mollis reprimique, ea vim habeo modus nonumes.' \
-             ' \nEa eligendi copiosae vis.' \
-             ' \nMolestie lobortis dignissim id eam, eum paulo quando primis ei.' \
-             ' \nAt qui ocurreret prodesset posidonium, quo putant platonem ea.'
-
-    write_file('test.i', s_test)
+    get_all_rod_pos()
